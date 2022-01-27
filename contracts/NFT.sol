2bfde1f365f1a74af639f2125d70a830d2d73b96
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-01-27 14:02:39 +0530

    Create NFTMarket contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address marketplaceAddress;
+
+    constructor (address marketplaceAddr) ERC721("Metaverse Tokens", "METT") {
+        marketplaceAddress = marketplaceAddr;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(marketplaceAddress, true);
+        return newItemId;
+    }
+}
---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+contract NFTMarket {
+    using Counters for Counters.Counter;
+    struct Auction {
+        address payable ownerAddress;
+        address tokenAddress;
+        uint tokenId;
+        uint minBidAmount;
+        uint highestBidAmount;
+        address payable highestBidder;
+        bool ended;
+        bool sold;
+    }
+
+    Counters.Counter auctionIds;
+    Auction[] auctions;
+
+    function submitAuction(
+        address tokenAddress,
+        uint tokenId,
+        uint minBidAmount
+    ) public returns (uint) {
+        IERC721 token = IERC721(tokenAddress);
+        address tokenOwner = token.ownerOf(tokenId);
+        require(tokenOwner == msg.sender, "You do not own the NFT");
+
+        Auction memory newAuction = Auction({
+            ownerAddress: payable(msg.sender),
+            tokenAddress: tokenAddress,
+            tokenId: tokenId,
+            minBidAmount: minBidAmount,
+            highestBidAmount: 0,
+            highestBidder: payable(0),
+            ended: false,
+            sold: false
+        });
+        uint newAuctionId = auctionIds.current();
+        auctions.push(newAuction);
+        return newAuctionId;
+    }
+
+    function submitBid(uint auctionId) public payable {
+        Auction memory auction = auctions[auctionId];
+        if (msg.value > auction.highestBidAmount) {
+            auction.highestBidder.transfer(auction.highestBidAmount);
+            auction.highestBidAmount = msg.value;
+            auction.highestBidder = payable(msg.sender);
+        }
+    }
+
+    function finalizeAuction(uint auctionId, bool accept) public {
+        Auction memory auction = auctions[auctionId];
+
+        if (accept) {
+            IERC721 token = IERC721(auction.tokenAddress);
+            token.safeTransferFrom(
+                auction.ownerAddress,
+                auction.highestBidder,
+                auction.tokenId
+            );
+            auction.ownerAddress.transfer(auction.highestBidAmount);
+            auction.sold = true;
+        } else {
+            auction.highestBidder.transfer(auction.highestBidAmount);
+            auction.sold = false;
+        }
+        auction.ended = true;
+    }
+}
