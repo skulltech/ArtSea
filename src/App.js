1439905e237e7f86bddc81ed3311d7fd9d4a500f
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-02-18 14:55:39 +0530

    Uninstall react-router, remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import { ConnectWallet } from "./components/ConnectWallet";
 import { MintNft } from "./components/MintNft";
 import {
