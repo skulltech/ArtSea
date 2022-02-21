0bc26e1323060e615ddf23bf530ce80f70a809f8
Sumit Ghosh <sumit@sumit-ghosh.com>
2022-02-21 13:42:36 +0530

    Fix CreateAuctionButton

--- src/components/CreateAuctionButton.js
+++ src/components/CreateAuctionButton.js
@@ -3,13 +3,44 @@ import { useForm } from "@mantine/hooks";
 import { useModals } from "@mantine/modals";
 import { useNotifications } from "@mantine/notifications";
 import { parseEther } from "ethers/lib/utils";
-import { useEffect, useState } from "react";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 import { IoAlertCircleSharp } from "react-icons/io5";
 import config from "../utils/config";
 import { getContract } from "../utils/utils";
 
-const CreateAuctionForm = ({ setFormValues, tokenAddress, tokenId }) => {
+const createAuction = async ({
+  currentAccount,
+  tokenAddress,
+  tokenId,
+  minBidAmount,
+}) => {
+  const marketContract = getContract({
+    currentAccount,
+    contractInfo: config.contracts.marketContract,
+  });
+  const txn = await marketContract.createAuction(
+    tokenAddress,
+    tokenId,
+    parseEther(minBidAmount.toString())
+  );
+  console.log("Transaction hash for creating auction:", txn.hash);
+  const receipt = await txn.wait();
+  console.log("Transaction receipt:", receipt);
+  const auctionCreatedEvent = receipt.events.filter((x) => {
+    return x.event === "AuctionCreated";
+  })[0];
+  const auctionId = auctionCreatedEvent.args[0].toString();
+  console.log("Transaction done, auction ID:", auctionId);
+};
+
+const CreateAuctionForm = ({
+  currentAccount,
+  tokenAddress,
+  tokenId,
+  closeModal,
+}) => {
+  const notifications = useNotifications();
+
   const form = useForm({
     initialValues: {
       minBidAmount: null,
@@ -18,12 +49,53 @@ const CreateAuctionForm = ({ setFormValues, tokenAddress, tokenId }) => {
     },
   });
 
-  useEffect(() => {
-    setFormValues(form.values);
-  }, [form.values, setFormValues]);
+  const handleFormSubmit = async (formValues) => {
+    console.log("formValues:", formValues);
+    const notificationId = notifications.showNotification({
+      loading: true,
+      title: "Creating Auction",
+      message: "Creating auction, please approve the Metamask transaction",
+      autoClose: false,
+      disallowClose: true,
+    });
+
+    try {
+      await createAuction({
+        currentAccount,
+        tokenAddress,
+        tokenId,
+        minBidAmount: formValues.minBidAmount,
+      });
+
+      notifications.updateNotification(notificationId, {
+        notificationId,
+        color: "teal",
+        title: "Auction created",
+        message: "Auction created",
+        icon: <BsFillCheckCircleFill />,
+        autoClose: false,
+        disallowClose: false,
+      });
+    } catch (error) {
+      notifications.updateNotification(notificationId, {
+        notificationId,
+        color: "red",
+        title: "Auction creation failed",
+        message: "An error occurred while creating auction",
+        icon: <IoAlertCircleSharp />,
+        autoClose: false,
+        disallowClose: false,
+      });
+    }
+  };
 
   return (
-    <form>
+    <form
+      onSubmit={form.onSubmit((formValues) => {
+        closeModal();
+        handleFormSubmit(formValues);
+      })}
+    >
       <Group direction="column" grow="true">
         <TextInput
           placeholder="NFT contract address"
@@ -46,6 +118,10 @@ const CreateAuctionForm = ({ setFormValues, tokenAddress, tokenId }) => {
           precision={18}
           {...form.getInputProps("minBidAmount")}
         />
+        <Group position="right">
+          <Button onClick={closeModal}>Cancel</Button>
+          <Button type="submit">Confirm</Button>
+        </Group>
       </Group>
     </form>
   );
@@ -58,73 +134,19 @@ export const CreateAuctionButton = ({
   buttonProps,
   children,
 }) => {
-  const [formValues, setFormValues] = useState(null);
-  const notifications = useNotifications();
   const modals = useModals();
 
-  const createAuction = async (formValues) => {
-    const notificationId = notifications.showNotification({
-      loading: true,
-      title: "Creating Auction",
-      message: "Creating auction, please approve the Metamask transaction",
-      autoClose: false,
-      disallowClose: true,
-    });
-
-    try {
-      const marketContract = getContract({
-        currentAccount,
-        contractInfo: config.contracts.marketContract,
-      });
-      const txn = await marketContract.createAuction(
-        formValues.tokenAddress,
-        formValues.tokenId,
-        parseEther(formValues.minBidAmount.toString())
-      );
-      console.log("Transaction hash for creating auction:", txn.hash);
-      const receipt = await txn.wait();
-      console.log("Transaction receipt:", receipt);
-      const auctionCreatedEvent = receipt.events.filter((x) => {
-        return x.event === "AuctionCreated";
-      })[0];
-      const auctionId = auctionCreatedEvent.args[0].toString();
-      console.log("Transaction done, auction ID:", auctionId);
-
-      notifications.updateNotification(notificationId, {
-        notificationId,
-        color: "teal",
-        title: "Auction created",
-        message: "Auction created",
-        icon: <BsFillCheckCircleFill />,
-        autoClose: false,
-        disallowClose: false,
-      });
-    } catch (error) {
-      console.log(error);
-
-      notifications.updateNotification(notificationId, {
-        notificationId,
-        color: "red",
-        title: "Auction creation failed",
-        message: "An error occurred while creating auction",
-        icon: <IoAlertCircleSharp />,
-        autoClose: false,
-        disallowClose: false,
-      });
-    }
-  };
-
   const openCreateAuctionModal = () => {
-    modals.openConfirmModal({
+    const modalId = modals.openModal({
       title: "Create Auction",
       children: (
         <CreateAuctionForm
-          setFormValues={setFormValues}
+          currentAccount={currentAccount}
           tokenAddress={tokenAddress}
           tokenId={tokenId}
+          closeModal={() => modals.closeModal(modalId)}
         />
       ),
-      onConfirm: () => createAuction(formValues),
     });
   };
 
